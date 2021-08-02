d6809ac126169ad5900c586efa8617e4cedd5724
ScionRs <grazs116@gmail.com>
2021-08-02 12:38:08 +0300

    Main page refactored

--- src/main/java/ru/ravilov/ClientBase/controller/HomeController.java
+++ src/main/java/ru/ravilov/ClientBase/controller/HomeController.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.ravilov.ClientBase.model.CarCategory;
+import ru.ravilov.ClientBase.model.MotorcycleCategory;
 import ru.ravilov.ClientBase.model.Segment;
-import ru.ravilov.ClientBase.service.CarCategoryService;
-import ru.ravilov.ClientBase.service.SegmentService;
+import ru.ravilov.ClientBase.model.TruckCategory;
+import ru.ravilov.ClientBase.service.*;
 
 import java.util.List;
 
@@ -16,12 +17,18 @@ public class HomeController {
 
     private SegmentService segmentService;
     private CarCategoryService carCategoryService;
+    private MotoCategoryService motoCategoryService;
+    private TruckCategoryService truckCategoryService;
 
     @Autowired
-    public HomeController(SegmentService segmentService, CarCategoryService carCategoryService) {
+    public HomeController(SegmentService segmentService, CarCategoryService carCategoryService, MotoCategoryService motoCategoryService, TruckCategoryService truckCategoryService) {
         this.segmentService = segmentService;
         this.carCategoryService = carCategoryService;
+        this.motoCategoryService = motoCategoryService;
+        this.truckCategoryService = truckCategoryService;
     }
+
+
 
 
 
@@ -29,7 +36,11 @@ public class HomeController {
     public String AllSegment(Model model){
 
         List<Segment> segmentList = segmentService.listAll();
+        List<MotorcycleCategory> motorcycleCategoryList = motoCategoryService.listAll();
         List<CarCategory> carCategoryList = carCategoryService.listAll();
+        List<TruckCategory> truckCategoryList = truckCategoryService.listAll();
+        model.addAttribute("truckCategoryList",truckCategoryList);
+        model.addAttribute("motorcycleCategoryList",motorcycleCategoryList);
         model.addAttribute("carCategoryList",carCategoryList);
         model.addAttribute("segmentList",segmentList);
         return "index";
